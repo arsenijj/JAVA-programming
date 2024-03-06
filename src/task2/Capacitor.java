d7af8ca357d0d4a61d236bc534763b8b2e11fa35
arsenijj <arsenijjnickitin2001@gmail.com>
2024-03-06 19:48:21 +0400

    fix | changed declarations in Main

--- src/task2/Capacitor.java
+++ src/task2/Capacitor.java
@@ -4,4 +4,5 @@ interface Capacitor {
     public void charge(double voltage);
     public void discharge();
     public void changeCapatiance(double capacitance);
+    public double getEnergy();
 }
--- src/task2/Main.java
+++ src/task2/Main.java
@@ -1,27 +1,91 @@
 package task2;
 
+import java.util.ArrayList;
+
 public class Main {
 
-    public void run(){
-        
-        StandardCapacitor cap1 = new StandardCapacitor(12.22, 14.44);
-        StandardCapacitor cap2 = new StandardCapacitor(12.22, 14.44); 
-        System.out.println(cap1.equals(cap2));    
-        System.out.println(cap1.hashCode());
-        System.out.println(cap1.compareTo(cap2));
-        ElectrolyticCapacitor cap3 = new ElectrolyticCapacitor(0, 0, 0);
-        ElectrolyticCapacitor cap4 = new ElectrolyticCapacitor(0, 0, 0);
-    
-        System.out.println(cap3.compareTo(cap4));
-    
-        // cap3.charge(12);
-        // cap3.maximumVoltage = 13;
-        // cap3.charge(12);
-        // System.out.println(cap3);
+    public void run() {
 
+        // StandardCapacitor
+        StandardCapacitor standardCapacitor1 = new StandardCapacitor(12.22, 14.44);
+        StandardCapacitor standardCapacitor2 = new StandardCapacitor(12.22, 14.44);
+
+        System.out.println(standardCapacitor1);
+        System.out.println(standardCapacitor2);
+
+        System.out.println("SC1 equals SC2: " + standardCapacitor1.equals(standardCapacitor2));
+
+        System.out.println("SC1 hashCode = " + standardCapacitor1.hashCode());
+        System.out.println("SC2 hashCode = " + standardCapacitor2.hashCode());
+
+        System.out.println("Compared SC1 & SC2: " + standardCapacitor1.compareTo(standardCapacitor2));
+
+        standardCapacitor2.voltage = 13;
+        System.out.println("Compared SC1 & SC2: " + standardCapacitor1.compareTo(standardCapacitor2));
+        standardCapacitor2.voltage = 15;
+        System.out.println("Compared SC1 & SC2: " + standardCapacitor1.compareTo(standardCapacitor2));
+
+        System.out.println(standardCapacitor1.getNameOfComponent());
+        System.out.println(standardCapacitor2.getNameOfComponent());
+
+        StandardCapacitor standardCapacitor1ShallowCopy = standardCapacitor1.shallowCopy();
+        StandardCapacitor standardCapacitor1DeepCopy = standardCapacitor1.deepCopy();
+
+        standardCapacitor1.charge(12);
+        standardCapacitor1.changeCapatiance(4);
+        System.out.println(standardCapacitor1);
+
+        standardCapacitor1.discharge();
+        System.out.println(standardCapacitor1);
+
+        // ElectrolyticCapacitor
+        ElectrolyticCapacitor electrolyticCapacitor1 = new ElectrolyticCapacitor(12.22, 14.44, 20);
+        ElectrolyticCapacitor electrolyticCapacitor2 = new ElectrolyticCapacitor(12.22, 14.44, 20);
+
+        System.out.println(electrolyticCapacitor1);
+        System.out.println(electrolyticCapacitor2);
+
+        System.out.println("SC1 equals SC2: " + electrolyticCapacitor1.equals(electrolyticCapacitor2));
+
+        System.out.println("SC1 hashCode = " + electrolyticCapacitor1.hashCode());
+        System.out.println("SC2 hashCode = " + electrolyticCapacitor2.hashCode());
+
+        System.out.println("Compared SC1 & SC2: " + standardCapacitor1.compareTo(electrolyticCapacitor2));
+
+        electrolyticCapacitor2.voltage = 13;
+        System.out.println("Compared SC1 & SC2: " + electrolyticCapacitor1.compareTo(electrolyticCapacitor2));
+        electrolyticCapacitor2.voltage = 15;
+        System.out.println("Compared SC1 & SC2: " + electrolyticCapacitor1.compareTo(electrolyticCapacitor2));
+
+        System.out.println(standardCapacitor1.getNameOfComponent());
+        System.out.println(standardCapacitor2.getNameOfComponent());
+
+        ElectrolyticCapacitor electrolyticCapacitor1ShallowCopy = electrolyticCapacitor1.shallowCopy();
+        ElectrolyticCapacitor electrolyticCapacitor1DeepCopy = electrolyticCapacitor1.deepCopy();
+
+        electrolyticCapacitor1DeepCopy.charge(12);
+        electrolyticCapacitor1DeepCopy.changeCapatiance(4);
+        System.out.println(standardCapacitor1);
+
+        electrolyticCapacitor1DeepCopy.discharge();
+        System.out.println(electrolyticCapacitor1DeepCopy);
+
+        ArrayList<Capacitor> a = new ArrayList<Capacitor>();
+        a.add(standardCapacitor1);
+        a.add(standardCapacitor2);
+        a.add(standardCapacitor1DeepCopy);
+        // a.add(standardCapacitor1ShallowCopy);
+        a.add(electrolyticCapacitor1);
+        a.add(electrolyticCapacitor2);
+        a.add(electrolyticCapacitor1DeepCopy);
+        // a.add(electrolyticCapacitor1ShallowCopy);
+        for (Capacitor cap : a) {
+            System.out.println(cap.getEnergy());
+        }
     }
+
     public static void main(String[] args) {
-    new Main().run();
-    
+        new Main().run();
+
     }
 }
